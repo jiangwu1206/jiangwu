60871f3ccd2c4df96b0ccfd35e90c992f8c852ae
jiangwu1206 <jiangwu1206@gmail.com>
2018-09-07 18:22:03 +0800

    update

--- script/orayddns.sh
+++ script/orayddns.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
-domain=jw1206.xicp.net
-orayUser=jw1206
-orayPassword=513029
-oldIP=$(host jw1206.xicp.net |awk '{print $4}')
-newIP=$(curl -s ifconfig.me)
-
-if [ "$oldIP"x != "$newIP"x ]
-then
-	echo "不相等"
-	curl http://$orayUser:$orayPassword@ddns.oray.com/ph/update?hostname=$domain&myip=$newIP
-else
-	echo 
+domain="domain"
+user="user"
+pwd="pwd"
+ip=`curl -s ip.tenfey.com`
+oldip=`host jw1206.xicp.net |awk '{print $NF}'`
+if [ "$ip"x != "$oldip"x ];then
+echo '不等'
+/usr/bin/curl "http://$user:$pwd@ddns.oray.com/ph/update?hostname=$domain"
 fi
+
+echo $ip
+echo $oldip
