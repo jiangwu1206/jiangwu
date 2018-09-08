4368dc0d733ca56949d0b4f58583f33501e1c71a
jiangwu1206 <jiangwu1206@gmail.com>
2018-09-08 10:17:59 +0800

    update

--- script/ups.py
+++ script/ups.py
@@ -8,10 +8,10 @@ for x in range(5):
     ret=os.system('ping -c 3 192.168.31.1')
     if(0 != ret):
         if(3<x):
-            os.system("systemctl stop mysqld")
-            os.system("systemctl stop redis")
-            os.system("supervisorctl stop all")
+            os.system("/usr/sbin/systemctl stop mysqld")
+            os.system("/usr/sbin/systemctl stop redis")
+            os.system("/usr/bin/supervisorctl stop all")
             time.sleep(5)
-            os.system("shutdown -P now")
+            os.system("/usr/sbin/shutdown -P now")
         continue
     break
