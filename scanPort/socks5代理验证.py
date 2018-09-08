46eff766b392b928f255142433c8b33c8a6d9ff0
jiangwu1206 <jiangwu1206@gmail.com>
2018-09-08 10:03:43 +0800

    add

--- scanPort/socks5代理验证.py
+++ scanPort/socks5代理验证.py
@@ -9,10 +9,11 @@ import threading
  
 #检查目标IP是否可用
 def check(ip,port=1080):
+    print(ip)
     if IsOpen(ip,port):
         try:
             if requests.head('http://ip.tenfey.com',timeout=3,proxies={'http':'socks5://%s:1080'%(ip),'https':'socks5://%s:1080'%(ip)}).ok :
-                print('有效IP:%s'%(ip))
+                print('===========有效socks5代理IP:%s=============='%(ip))
                 postdata={}
                 postdata['ip']=ip.strip()
                 print(requests.post('http://ipcheck.tenfey.com/proxyipinsert/',data=postdata).text)
@@ -21,30 +22,14 @@ def check(ip,port=1080):
         except:
             return 0
 
-#迭代IP段
-def forIPy(iprange):
-    try:
-        ipl=IPy.IP(iprange)
-        for ip in ipl:
-            check(str(ip).strip())
-            #threading.Thread(target=check,args=(str(ip).strip(),)).start()
-            #time.sleep(0.0005)
-
-    except Exception as e:
-        print("forIPy",e)
 
 if __name__=='__main__':
     print(time.time())
     file=open('1080.txt','r')
-    pool=multiprocessing.Pool()
-    for iprange in file:
-        #forIPy(iprange.strip())
-        #pool.apply_async(forIPy,args=(iprange.strip(),))
-        threading.Thread(target=forIPy,args=(iprange.strip(),)).start()
+    for ip in file:
+        threading.Thread(target=check,args=(ip.strip(),)).start()
         time.sleep(0.0005)
     file.close()
-    pool.close()
-    pool.join()
     del file
     print(time.time())
     input("继续")
